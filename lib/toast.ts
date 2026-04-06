53f05760a64e9c76b6347329d2ddb04452fcc950
usernameunknown114-cellenme <usernameunknown114@gmail.com>
2026-04-06 21:49:34 +0800

    Create toast.ts

---
+++ lib/toast.ts
@@ -0,0 +1,9 @@
+import toast, { Toaster } from 'react-hot-toast';
+
+export const showToast = {
+  success: (message: string) => toast.success(message),
+  error: (message: string) => toast.error(message),
+  loading: (message: string) => toast.loading(message),
+};
+
+export { Toaster };
